e3640574bcdc8c93565cd23994008a9ebc2e45a8
RudyChemik <klefedronowelove@gmail.com>
2025-10-29 23:15:23 +0100

    feat: error page

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,12 +2,12 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
 
-export default function RootLayout() {
+export default function RootLayout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
         <main className="flex-1 w-full h-auto">
-          <Outlet />
+          {children ? children : <Outlet />}
         </main>
       <Footer /> 
     </div>
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,9 +1,21 @@
+import React, { useRef } from "react";
+import Lottie from "lottie-react";
+import NotFoundLottie from "@/assets/lotties/404.json";
 
 export default function Error() {
+  const lottieRef = useRef();
+
   return (
-    <div className="w-full h-auto flex items-center justify-center">
-      <div className="w-full h-screen">
-        <p className="text-lg text-center">CHUJ ERROR VIEW ERROR CHUJ</p>
+    <div className="w-full h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-7xl px-6 flex flex-col items-center text-center">
+        <Lottie
+          lottieRef={lottieRef}
+          animationData={NotFoundLottie}
+          loop
+          autoplay
+          className="w-3/4 max-w-[500px] md:max-w-[700px] aspect-square -mt-64"
+        />
+        <p className="mt-6 text-2xl md:text-4xl lg:text-5xl font-marker tracking-widest"> Tego to jeszcze nie zbudowalismy... </p>
       </div>
     </div>
   );
