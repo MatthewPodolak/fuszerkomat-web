78f142fd39d53e37de917b5aa98f03a31b78ccc3
Mateusz <144284564+MatthewPodolak@users.noreply.github.com>
2025-10-31 16:34:52 +0100

    feat: company dashboard (#19)

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import RootLayout from "./layouts/RootLayout";
 import PublicOnly from "@/layouts/guards/PublicOnly.jsx";
 import AuthOnly from "@/layouts/guards/PublicOnly.jsx";
 import UserOnly from "@/layouts/guards/UserOnly.jsx";
+import CompanyOnly from "@/layouts/guards/CompanyOnly.jsx";
 import { hydrateTokenStore } from "@/api/tokenStore.js";
 import { ToastProvider } from "@/context/ToastContext.jsx";
 import GlobalErrorCatcher from "@/helpers/GlobalErrorCatcher.jsx";
@@ -22,6 +23,7 @@ const Register = lazy(() => import("./pages/auth/Register"));
 const Login = lazy(() => import("./pages/auth/Login"));
 
 const UserDashboard = lazy(() => import("./pages/user/UserDashboard"));
+const CompanyDashboard = lazy(() => import("./pages/company/CompanyDashboard"));
 
 const router = createBrowserRouter([
   {
@@ -33,7 +35,8 @@ const router = createBrowserRouter([
       { path: "category/:category", element: <Category /> },
       { path: "register", element: <PublicOnly><Register /></PublicOnly> },
       { path: "login",    element: <PublicOnly><Login /></PublicOnly> },
-      { path: "/user/dashboard", element: <UserOnly><UserDashboard /></UserOnly>}
+      { path: "/user/dashboard", element: <UserOnly><UserDashboard /></UserOnly>},
+      { path: "/company/dashboard", element: <CompanyOnly><CompanyDashboard /></CompanyOnly>}
     ],
   },
 ]);
