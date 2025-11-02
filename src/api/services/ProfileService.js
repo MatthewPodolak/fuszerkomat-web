8f92af4e2b8d42ad2625a7a5fff54e0c34e87ec9
RudyChemik <klefedronowelove@gmail.com>
2025-11-02 12:44:13 +0100

    feat: navbar pfp display

--- src/api/services/ProfileService.js
+++ src/api/services/ProfileService.js
@@ -1,6 +1,8 @@
 import { apiJson, apiForm } from "@/api/api.js";
 import { API } from "@/api/endpoints.js";
 
+const KEY_PFP = "profile.pfp";
+
 export const ProfileService = {
   async getOwn( { ct, timeoutMs } = {}) {
     const { method, url } = API.account.getOwnProfile;
@@ -12,6 +14,21 @@ export const ProfileService = {
       },
       { ct, timeoutMs }
     );
+
+    if(res.status === 200)
+    { 
+      const { userProfileDataVMO, companyProfileDataVMO } = res.data;
+
+      if (!userProfileDataVMO && !companyProfileDataVMO) return res;
+
+      const profileData = userProfileDataVMO ?? companyProfileDataVMO;
+      const pfp = profileData?.img;
+
+      if (pfp) {
+        const fullUrl = import.meta.env.VITE_API_ORIGIN + pfp;
+        localStorage.setItem(KEY_PFP, fullUrl);
+      }
+    }
 
     return res;
   },
@@ -29,5 +46,15 @@ export const ProfileService = {
     );
 
     return res;
+  },
+
+  async getProfilePicture() {
+    let storedPfp = localStorage.getItem(KEY_PFP);
+    if (!storedPfp || storedPfp === "null" || storedPfp === "undefined"){
+      await this.getOwn();
+      storedPfp = localStorage.getItem(KEY_PFP);
+    }
+
+    return storedPfp;
   },
 };
