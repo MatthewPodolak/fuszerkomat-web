433155cd96825fd87e1a63643912748c359e22ab
RudyChemik <klefedronowelove@gmail.com>
2025-11-15 21:13:20 +0100

    fix: home page linking

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,11 +4,21 @@ import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { removePolishChars } from "../helpers/textHelper.js";
 
+import Questionary from "@/components/Questionary";
+
 export default function Category() {
 
     const navigate = useNavigate();
     const { category } = useParams();
     const [selectedCategory, setSelectedCategory] = useState(null);
+    const [isQuestOpen, setIsQuestOpen] = useState(false);
+    const [preQuest, setPreQuest] = useState(null);
+
+    const selectTag = (tag) => {
+        let prev = { cat: selectedCategory.category, tag: tag };
+        setPreQuest(prev);
+        setIsQuestOpen(true);
+    };
 
     useEffect(() => {
         if(!category){ navigate("/error"); }
@@ -24,22 +34,25 @@ export default function Category() {
     if (!selectedCategory) return null;
 
     return (
-        <div className="w-full h-auto min-h-screen flex items-center bg-whitesmoke justify-center">
-            <div className="w-7xl h-auto flex flex-col">
-                <p className="text-5xl text-left font-marker tracking-widest">{removePolishChars(selectedCategory.name)}</p>
-                <p className="text-xl text-left font-helvetica tracking-wide ml-2 mt-6">{selectedCategory.desc}</p>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 py-2 mb-12 mt-12">
-                    {selectedCategory.tags.map((tag) => {
-                        return (
-                            <Link key={tag.name} className="card h-42 bg-base-100 hover:shadow-lg transition cursor-pointer">
-                                <div className="card-body">
-                                    <h3 className="card-title text-base">{tag.name}</h3>
+        <>
+            <div className="w-full h-auto min-h-screen flex items-center bg-whitesmoke justify-center">
+                <div className="w-7xl h-auto flex flex-col">
+                    <p className="text-5xl text-left font-marker tracking-widest">{removePolishChars(selectedCategory.name)}</p>
+                    <p className="text-xl text-left font-helvetica tracking-wide ml-2 mt-6">{selectedCategory.desc}</p>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 py-2 mb-12 mt-12">
+                        {selectedCategory.tags.map((tag) => {
+                            return (
+                                <div onClick={() => selectTag(tag.tagName)} key={tag.name} className="card h-42 bg-base-100 hover:shadow-lg transition cursor-pointer">
+                                    <div className="card-body">
+                                        <h3 className="card-title text-base">{tag.name}</h3>
+                                    </div>
                                 </div>
-                            </Link>
-                        );
-                    })}
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+            <Questionary open={isQuestOpen} onClose={() => setIsQuestOpen(false)} pre={preQuest} />
+        </>
     );
 }
